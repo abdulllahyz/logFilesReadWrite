78ea20be8b5423e6cb0776e666f5af8280a12527
Barbaros Yildiz <barbaros.yildiz@gameduell.de>
2019-02-24 20:23:54 +0100

    bazi tavsiyeler

--- src/main/java/com/abdullah/App.java
+++ src/main/java/com/abdullah/App.java
@@ -1,63 +1,92 @@
 package com.abdullah;
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args ) {
+public class App {
 
+  /**
+   * Bu uygulama dizini parametre olarak alir. java -jar target/log-1.0-SNAPSHOT.jar
+   * com.abdullah.App dizinin_path
+   */
+  public static void main(String args[]) {
 
-        try (Scanner scanner = new Scanner(new BufferedReader(new FileReader("C:\\Users\\wind_\\IdeaProjects\\log\\src\\main\\java\\com\\abdullah\\data.log")));
-             BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("C:\\Users\\wind_\\IdeaProjects\\log\\src\\main\\java\\com\\abdullah\\dataLogRecords.log"))) {
+    ///Users/byil/Downloads/root
+    String rootPath = args[0];
 
+    //dizinleri listelicem
+    //her bir dizin icin dosyalari okuyup icindeki kelimeleri bulacam (dizini icindeki .log uzantili dosyalari okuyup top 3 kelimeyi bulsa)
+    //her bir gun icin top 3 kelimeyi bulacam sonra dosyaya yazdiracaz.
 
-            while (scanner.hasNextLine()) {
+    List<String> directories = list(rootPath);
+    String directory = "/Users/byil/Downloads/root/2018-01-04";
+    List<String> words = readAllWords(directory);
 
-                String logRecords = scanner.nextLine();
-                String[] logArray = logRecords.split(" ");
+    try (Scanner scanner = new Scanner(new BufferedReader(new FileReader(
+        "C:\\Users\\wind_\\IdeaProjects\\log\\src\\main\\java\\com\\abdullah\\data.log")));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(
+            "C:\\Users\\wind_\\IdeaProjects\\log\\src\\main\\java\\com\\abdullah\\dataLogRecords.log"))) {
 
+      while (scanner.hasNextLine()) {
 
-                int count;
+        String logRecords = scanner.nextLine();
+        String[] logArray = logRecords.split(" ");
 
-                HashMap<String, Integer> map = new HashMap<String, Integer>();
+        int count;
 
-                for (String word : logArray) {
-                    if (map.containsKey(word)) {
-                        count = map.get(word);
-                        map.put(word, count + 1);
-                    } else {
-                        map.put(word, 1);
-                    }
-                }
+        HashMap<String, Integer> map = new HashMap<String, Integer>();
+
+        for (String word : logArray) {
+          if (map.containsKey(word)) {
+            count = map.get(word);
+            map.put(word, count + 1);
+          } else {
+            map.put(word, 1);
+          }
+        }
 
 //                for (Map.Entry<String, Integer> entry : map.entrySet()) {
 //                    System.out.println(entry.getKey() + " : " + entry.getValue());
 //                }
 
-                List<Map.Entry<String, Integer>> list = new LinkedList<Map.Entry<String, Integer>>(map.entrySet());
+        List<Map.Entry<String, Integer>> list = new LinkedList<Map.Entry<String, Integer>>(
+            map.entrySet());
 
-                Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
-                    public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
-                        return (o1.getValue()).compareTo(o2.getValue());
-                    }
-                });
+        Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
+          public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+            return (o1.getValue()).compareTo(o2.getValue());
+          }
+        });
 
-
-                Map<String, Integer> map1 = new LinkedHashMap<String, Integer>();
-                for (Map.Entry<String, Integer> entry : list) {
-                    map1.put(entry.getKey(), entry.getValue());
-                }
+        Map<String, Integer> map1 = new LinkedHashMap<String, Integer>();
+        for (Map.Entry<String, Integer> entry : list) {
+          map1.put(entry.getKey(), entry.getValue());
+        }
 //
 //                for (Map.Entry<String, Integer> entry : map.entrySet()) {
 //                    String key = entry.getKey();
 //                    int value = entry.getValue();
 //                    System.out.println(key + " : " + value);
-
 
 //
 //                for (Map.Entry<String, Integer> entry : list
@@ -69,23 +98,19 @@ public class App
 
 //                System.out.println(list.size());
 
+        for (int i = list.size() - 3; i < list.size(); i++) {
 
-                for (int i = list.size()-3; i <list.size() ; i++) {
-
-
-                    bufferedWriter.write(String.valueOf(list.get(i))+" ");
+          bufferedWriter.write(String.valueOf(list.get(i)) + " ");
 
 
-                }
+        }
 
-                System.out.println("====================");
-                System.out.println(  list.get(list.size()-3));
-                System.out.println(  list.get(list.size()-2));
-                System.out.println(list.get(list.size()-1));
+        System.out.println("====================");
+        System.out.println(list.get(list.size() - 3));
+        System.out.println(list.get(list.size() - 2));
+        System.out.println(list.get(list.size() - 1));
 
 //
-
-
 
 //                for (int i = map1.size()-3; i < map1.size() ; i++) {
 //                   System.out.print(map1.get(i)+",");
@@ -95,19 +120,62 @@ public class App
 //
 //                }
 
-
 //        } catch (FileNotFoundException e) {
 //            e.printStackTrace();
 //        } catch (IOException e) {
 //            e.printStackTrace();
 //        }
 
+      }
+    } catch (FileNotFoundException e1) {
+      e1.printStackTrace();
+    } catch (IOException e1) {
+      e1.printStackTrace();
+    }
+  }
 
-            }
-        } catch (FileNotFoundException e1) {
-            e1.printStackTrace();
-        } catch (IOException e1) {
-            e1.printStackTrace();
+  /**
+   * @param directory Dosyanin pathi
+   * @return
+   */
+  public static List<String> readAllWords(String directory) {
+    List<String> words = new ArrayList<>();
+    Path path = Paths.get(directory);
+    try {
+      List<String> lines = Files.readAllLines(path);
+      for (String line : lines) {
+        String[] wordArray = line.split("\\s+");//regular expression bak
+        for (int i = 0; i < wordArray.length; i++) {
+          String clean = wordArray[i]
+              .toLowerCase()
+              .replace(".", "")
+              .replace(",", "")
+              .replace("?", "");
+          words.add(clean);
         }
+      }
+      return words;
+    } catch (IOException e) {
+      e.printStackTrace();
+      return null;
     }
+  }
+
+  public static List<String> list(String rootPath) {
+    List<String> files = new ArrayList<>();
+
+    try {
+      DirectoryStream<Path> paths = Files.newDirectoryStream(Paths.get(rootPath));
+      paths.forEach(directory -> {
+        String dizin = directory.toString();
+        if (!dizin.contains(".DS_Store")) {
+          files.add(dizin);
+        }
+      });
+      return files;
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+    return files;
+  }
+}
--- src/test/java/com/abdullah/AppTest.java
+++ src/test/java/com/abdullah/AppTest.java
@@ -2,19 +2,36 @@ package com.abdullah;
 
 import static org.junit.Assert.assertTrue;
 
+import java.util.List;
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+public class AppTest {
+
+  /**
+   * Rigorous Test :-)
+   */
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+
+  @Test
+  public void name() {
+    App app = new App();
+    List<String> list = app.list("/Users/byil/Downloads/root");
+    Assert.assertFalse(list.isEmpty());
+
+  }
+
+  @Test
+  public void dosyaOkuma() {
+    App app = new App();
+    List<String> words = app.readAllWords("/Users/byil/Downloads/root/2018-01-12/data5.log");
+    Assert.assertFalse(words.isEmpty());
+  }
 }
+
